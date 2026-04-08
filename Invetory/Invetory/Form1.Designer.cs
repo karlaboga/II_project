b0b7dba2ad23ef8bd94edee95811ec6c5573cf93
timeeagota-ship-it <timeeagota@gmail.com>
2026-04-08 14:22:38 +0300

    Add files via upload

---
+++ Invetory/Invetory/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace Invetory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Close = new System.Windows.Forms.Button();
+            this.textBox_Product = new System.Windows.Forms.TextBox();
+            this.label_Product = new System.Windows.Forms.Label();
+            this.comboBox_Category = new System.Windows.Forms.ComboBox();
+            this.numericUpDown_Quantity = new System.Windows.Forms.NumericUpDown();
+            this.label_Category = new System.Windows.Forms.Label();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Delete = new System.Windows.Forms.Button();
+            this.label_Quantity = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button_Edit = new System.Windows.Forms.Button();
+            this.textBox_Unit = new System.Windows.Forms.TextBox();
+            this.label_Unit = new System.Windows.Forms.Label();
+            this.numericUpDown_Min_Stock = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Quantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Min_Stock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(783, 12);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(29, 26);
+            this.button_Close.TabIndex = 0;
+            this.button_Close.Text = "X";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // textBox_Product
+            // 
+            this.textBox_Product.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Product.Location = new System.Drawing.Point(144, 120);
+            this.textBox_Product.Name = "textBox_Product";
+            this.textBox_Product.Size = new System.Drawing.Size(208, 31);
+            this.textBox_Product.TabIndex = 2;
+            // 
+            // label_Product
+            // 
+            this.label_Product.AutoSize = true;
+            this.label_Product.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Product.Location = new System.Drawing.Point(52, 123);
+            this.label_Product.Name = "label_Product";
+            this.label_Product.Size = new System.Drawing.Size(86, 25);
+            this.label_Product.TabIndex = 3;
+            this.label_Product.Text = "Product";
+            // 
+            // comboBox_Category
+            // 
+            this.comboBox_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox_Category.FormattingEnabled = true;
+            this.comboBox_Category.Items.AddRange(new object[] {
+            "Meat",
+            "Dairy",
+            "Veggies",
+            "Fruits",
+            "Spices",
+            "All"});
+            this.comboBox_Category.Location = new System.Drawing.Point(144, 169);
+            this.comboBox_Category.Name = "comboBox_Category";
+            this.comboBox_Category.Size = new System.Drawing.Size(208, 33);
+            this.comboBox_Category.TabIndex = 4;
+            this.comboBox_Category.SelectedIndexChanged += new System.EventHandler(this.comboBox_Category_SelectedIndexChanged);
+            // 
+            // numericUpDown_Quantity
+            // 
+            this.numericUpDown_Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown_Quantity.Location = new System.Drawing.Point(144, 227);
+            this.numericUpDown_Quantity.Name = "numericUpDown_Quantity";
+            this.numericUpDown_Quantity.Size = new System.Drawing.Size(208, 31);
+            this.numericUpDown_Quantity.TabIndex = 5;
+            // 
+            // label_Category
+            // 
+            this.label_Category.AutoSize = true;
+            this.label_Category.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Category.Location = new System.Drawing.Point(39, 177);
+            this.label_Category.Name = "label_Category";
+            this.label_Category.Size = new System.Drawing.Size(99, 25);
+            this.label_Category.TabIndex = 6;
+            this.label_Category.Text = "Category";
+            // 
+            // button_Add
+            // 
+            this.button_Add.BackColor = System.Drawing.Color.YellowGreen;
+            this.button_Add.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Add.Location = new System.Drawing.Point(418, 138);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(90, 44);
+            this.button_Add.TabIndex = 7;
+            this.button_Add.Text = "Add";
+            this.button_Add.UseVisualStyleBackColor = false;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // button_Delete
+            // 
+            this.button_Delete.BackColor = System.Drawing.Color.IndianRed;
+            this.button_Delete.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Delete.Location = new System.Drawing.Point(418, 238);
+            this.button_Delete.Name = "button_Delete";
+            this.button_Delete.Size = new System.Drawing.Size(90, 44);
+            this.button_Delete.TabIndex = 8;
+            this.button_Delete.Text = "Delete";
+            this.button_Delete.UseVisualStyleBackColor = false;
+            this.button_Delete.Click += new System.EventHandler(this.button_Delete_Click);
+            // 
+            // label_Quantity
+            // 
+            this.label_Quantity.AutoSize = true;
+            this.label_Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Quantity.Location = new System.Drawing.Point(46, 233);
+            this.label_Quantity.Name = "label_Quantity";
+            this.label_Quantity.Size = new System.Drawing.Size(92, 25);
+            this.label_Quantity.TabIndex = 9;
+            this.label_Quantity.Text = "Quantity";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 384);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(643, 197);
+            this.dataGridView1.TabIndex = 10;
+            //this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // button_Edit
+            // 
+            this.button_Edit.BackColor = System.Drawing.Color.PowderBlue;
+            this.button_Edit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Edit.Location = new System.Drawing.Point(418, 188);
+            this.button_Edit.Name = "button_Edit";
+            this.button_Edit.Size = new System.Drawing.Size(90, 44);
+            this.button_Edit.TabIndex = 11;
+            this.button_Edit.Text = "Edit";
+            this.button_Edit.UseVisualStyleBackColor = false;
+            this.button_Edit.Click += new System.EventHandler(this.button_Edit_Click);
+            // 
+            // textBox_Unit
+            // 
+            this.textBox_Unit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_Unit.Location = new System.Drawing.Point(144, 278);
+            this.textBox_Unit.Name = "textBox_Unit";
+            this.textBox_Unit.Size = new System.Drawing.Size(208, 31);
+            this.textBox_Unit.TabIndex = 12;
+            // 
+            // label_Unit
+            // 
+            this.label_Unit.AutoSize = true;
+            this.label_Unit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Unit.Location = new System.Drawing.Point(88, 284);
+            this.label_Unit.Name = "label_Unit";
+            this.label_Unit.Size = new System.Drawing.Size(50, 25);
+            this.label_Unit.TabIndex = 13;
+            this.label_Unit.Text = "Unit";
+            // 
+            // numericUpDown_Min_Stock
+            // 
+            this.numericUpDown_Min_Stock.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown_Min_Stock.Location = new System.Drawing.Point(144, 333);
+            this.numericUpDown_Min_Stock.Name = "numericUpDown_Min_Stock";
+            this.numericUpDown_Min_Stock.Size = new System.Drawing.Size(208, 31);
+            this.numericUpDown_Min_Stock.TabIndex = 14;
+            this.numericUpDown_Min_Stock.ValueChanged += new System.EventHandler(this.numericUpDown_Min_Stock_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 339);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Min_Stock";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Add - complete all the text box(Produc, Category etc) ",
+            "Edit - write Product name then change as desired",
+            "Delete - write Product name",
+            "At the end of each operation press coresponding button"});
+            this.listBox1.Location = new System.Drawing.Point(514, 188);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 56);
+            this.listBox1.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(346, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 31);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Inventory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 657);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown_Min_Stock);
+            this.Controls.Add(this.label_Unit);
+            this.Controls.Add(this.textBox_Unit);
+            this.Controls.Add(this.button_Edit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label_Quantity);
+            this.Controls.Add(this.button_Delete);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.label_Category);
+            this.Controls.Add(this.numericUpDown_Quantity);
+            this.Controls.Add(this.comboBox_Category);
+            this.Controls.Add(this.label_Product);
+            this.Controls.Add(this.textBox_Product);
+            this.Controls.Add(this.button_Close);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Quantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Min_Stock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Close;
+        private System.Windows.Forms.TextBox textBox_Product;
+        private System.Windows.Forms.Label label_Product;
+        private System.Windows.Forms.ComboBox comboBox_Category;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Quantity;
+        private System.Windows.Forms.Label label_Category;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Delete;
+        private System.Windows.Forms.Label label_Quantity;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn col_Name;
+        private System.Windows.Forms.Button button_Edit;
+        private System.Windows.Forms.TextBox textBox_Unit;
+        private System.Windows.Forms.Label label_Unit;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Min_Stock;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
