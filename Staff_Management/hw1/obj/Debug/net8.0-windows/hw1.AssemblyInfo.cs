f31d64d67919a518cfe72158f75d212208011edb
karlaboga <karlamaria.boga@gmail.com>
2026-04-25 03:05:01 +0300

    Am schimbat niste pathuri

--- Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
+++ Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+728ba899e54f1548c31eb8ef4511980585fa40b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
