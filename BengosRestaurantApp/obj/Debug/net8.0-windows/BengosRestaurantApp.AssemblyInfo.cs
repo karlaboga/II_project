0c4949b5ec86882d59f71593fd8cbd169667d386
tdr-diana <toader.diana.patricia@gmail.com>
2026-05-07 00:42:33 +0300

    dnfchgjvhkbhlk

--- BengosRestaurantApp/obj/Debug/net8.0-windows/BengosRestaurantApp.AssemblyInfo.cs
+++ BengosRestaurantApp/obj/Debug/net8.0-windows/BengosRestaurantApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BengosRestaurantApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7000798f4522c90e5afdf8619334bea6079bc0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ad11e8fdeb527a0ef85d3f06ceedd6d878f8325")]
 [assembly: System.Reflection.AssemblyProductAttribute("BengosRestaurantApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BengosRestaurantApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
