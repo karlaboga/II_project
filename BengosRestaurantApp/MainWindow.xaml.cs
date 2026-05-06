51247e236e88b59ef04b8231ab081620406cb6ad
tdr-diana <toader.diana.patricia@gmail.com>
2026-05-06 20:08:53 +0300

    Update web app port to 8080 for Docker/IIS Express
    - WPF app now launches web app on http://localhost:8080
    - Port matches IIS Express config (applicationhost.config)
    - Web app accessible at http://localhost:8080

--- BengosRestaurantApp/MainWindow.xaml.cs
+++ BengosRestaurantApp/MainWindow.xaml.cs
@@ -34,20 +34,22 @@ namespace BengosRestaurantApp
 
         private void BtnMenu_Click(object sender, RoutedEventArgs e)
         {
-            // Launch the ASP.NET MVC web app (DigitalClientMenu)
-            var webAppPath = System.IO.Path.Combine(
-                System.AppDomain.CurrentDomain.BaseDirectory,
-                "..\\..\\..\\..\\DigitalClientMenu\\BengosMenu\\BengosMenu.csproj");
-
-            // Try to open in Visual Studio or run with dotnet
+            // Launch the ASP.NET MVC web app (DigitalClientMenu) on IIS Express port 8080
             try
             {
                 Process.Start(new ProcessStartInfo
                 {
-                    FileName = "https://localhost:44300", // Default IIS Express port
+                    FileName = "http://localhost:8080", // IIS Express port from applicationhost.config
                     UseShellExecute = true
                 });
             }
+            catch
+            {
+                MessageBox.Show("Please open the DigitalClientMenu project in Visual Studio and run it.\n\n" +
+                    "The web app should run on: http://localhost:8080",
+                    "Web App Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
             catch
             {
                 MessageBox.Show("Please open the DigitalClientMenu project in Visual Studio and run it.",
