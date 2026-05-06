01fe85eb1a96cc8876de369b2176582b8fb12de2
timeeagota-ship-it <timeeagota@gmail.com>
2026-05-06 14:20:09 +0300

    inventarWPF

--- Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
+++ Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f31d64d67919a518cfe72158f75d212208011edb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0da83671d1b7b8c1b6effcd60899dd78d5f0954")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ inventoryWPF/inventoryWPF/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inventoryWPF
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private int _id;
+        private string _productName = string.Empty;
+        private string _category = string.Empty;
+        private int _quantity;
+        private string _unit = string.Empty;
+        private int _minStock;
+
+        public int ID { get => _id; set { _id = value; OnPropertyChanged(); } }
+        public string ProductName { get => _productName; set { _productName = value; OnPropertyChanged(); } }
+        public string Category { get => _category; set { _category = value; OnPropertyChanged(); } }
+        public int Quantity { get => _quantity; set { _quantity = value; OnPropertyChanged(); } }
+        public string Unit { get => _unit; set { _unit = value; OnPropertyChanged(); } }
+        public int Min_Stock { get => _minStock; set { _minStock = value; OnPropertyChanged(); } }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
