0886c7163f29ca17ce8f2c3ad8c7e3cc4548aebe
timeeagota-ship-it <timeeagota@gmail.com>
2026-05-10 18:55:21 +0300

    UML class diagram

--- Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
+++ Staff_Management/hw1/obj/Debug/net8.0-windows/hw1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hw1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0da83671d1b7b8c1b6effcd60899dd78d5f0954")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01fe85eb1a96cc8876de369b2176582b8fb12de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("hw1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hw1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
