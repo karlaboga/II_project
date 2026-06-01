5b4ce6c2686990c3808782b641906a1f68ab2fea
timeeagota-ship-it <timeeagota@gmail.com>
2026-06-01 19:33:22 +0300

    Active Orders Window
    Active Orders Window FUNCTIONAL

---
+++ BengosApplication/Kitchen/Models1/KitchenItem.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+namespace Kitchen.Models1
+{
+    class KitchenItem
+    {
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+    }
+}
